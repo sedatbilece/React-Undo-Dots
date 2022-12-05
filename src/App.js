e94ef4d93dd715b27d1d623a7676ea4d95eef44b
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-12-05 16:40:47 +0300

    do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [points, setPoints] = useState([]);
+  const [data, setData] = useState([]);
+  const clickHandle = (e) => {
+    console.log(e.clientX, e.clientY);
+    setPoints(points => [...points, 
+      {
+        x: e.clientX,
+        y: e.clientY
+      }
+    ]); 
+    setData([])
+  }
+  const redoHandle = (e) => {
+    e.stopPropagation();
+    const data =[...points];
+    const item =data.pop();
+    setData(data => [...data, item]); 
+    setPoints(data);
+  }
+  const undoHandle = (e) => {
+    e.stopPropagation();
+    const d=[...data];
+    const item =d.pop();
+    setPoints(points => [...points, item]);
+    setData(d);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="screen" onClick={clickHandle}>
+      <header className='header'>
+        <button onClick={redoHandle} disabled ={points.length==0}>redo</button>
+        <button onClick={undoHandle} disabled = {data.length==0}>undo</button>
       </header>
+      {points.map((point, index) => (
+        <div className="point" key={index} style={{top: point.y, left: point.x}}></div>
+      ))}
     </div>
   );
 }
